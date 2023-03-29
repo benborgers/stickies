0af36fe72c4d9ce54d8e0e38409e9463d708a232
Ben Borgers <borgersbenjamin@gmail.com>
2023-03-28 22:15:28 -0400

    Install React and Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+export default defineConfig({
+  integrations: [tailwind(), react()],
+});
