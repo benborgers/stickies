2a61893ebbabeabb36902252c120b1a89bb6735b
Ben Borgers <borgersbenjamin@gmail.com>
2023-04-15 09:42:49 -0400

    Remove flash of auth screen

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -8,12 +8,14 @@ const user = atom<null | User>(null);
 export default user;
 
 export async function refreshUser() {
+  user.set(pb.authStore.model as unknown as User);
+
   try {
     // This will throw if the user token is no longer valid.
-    // If so, we just don’t set the user and they have to log in again.
     await pb.collection("users").authRefresh();
-    user.set(pb.authStore.model as unknown as User);
-  } catch {}
+  } catch {
+    user.set(null);
+  }
 }
 
 onMount(user, () => {
